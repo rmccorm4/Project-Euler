6c020bb70bbac51a5e6ee5e084a14873aaa12378
Ryan McCormick <rmccorm4@binghamton.edu>
2018-04-27 16:05:14 -0400

    finish #10 in c++

--- 10/main.cpp
+++ 10/main.cpp
@@ -2,33 +2,30 @@
 #include <cstdint>
 using namespace std;
 
-int main() {
-	const int NUM_PRIMES = 1000;
-	int primes[NUM_PRIMES];
-	primes[0] = 2;
-	int count = 1;
-	int i = 3;
-	while (count < NUM_PRIMES) {
-		for (int j = 2; j < i; j++) {
-			if (j >= 2000000) {
-				uint64_t sum = 0;
-				for (int i = 0; i < count; i++)
-				{
-					sum += primes[count];
-				}
-				cout << sum << endl;
-				// Done, found desired sum
-				exit(0);
-			}
-			if (i % j == 0) {
-				break;
-			}
-			if (j == i-1) {
-				primes[count] = j+1;
-				count++;
-			}
+#define BOUND 2000000
+
+bool isPrime(int n)
+{
+	int range = n;
+	for (int i = 2; i < range; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
 		}
-		i++;
+		range = n / i;
 	}
-	cout << primes[count] << endl;
+	return true;
+}
+
+int main() 
+{
+	uint64_t sum = 0;
+	for (int i = 2; i < BOUND; i++)
+	{
+		if(isPrime(i))
+			sum += i;
+	}
+	cout << sum << endl;
+	return 0;
 }
